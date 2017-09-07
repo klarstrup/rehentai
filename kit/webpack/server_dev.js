b88f2ca67cb0040371f0e65613ce07b0fe16478f
klarstrup <jonas@klarstrup.dk>
2017-09-07 22:50:15 +0200

    Upgraded project to ReactQL kit v2.8.4

--- kit/webpack/server_dev.js
+++ kit/webpack/server_dev.js
@@ -72,6 +72,21 @@ export default [
     },
 
     plugins: [
+      new webpack.DefinePlugin({
+        // We ARE running on the server
+        SERVER: true,
+        'process.env': {
+          // Point the server host/port to the production server
+          HOST: JSON.stringify(process.env.HOST || 'localhost'),
+          PORT: JSON.stringify(process.env.PORT || '8081'),
+          SSL_PORT: process.env.SSL_PORT ? JSON.stringify(process.env.SSL_PORT) : null,
+
+          // Debug development
+          NODE_ENV: JSON.stringify('development'),
+          DEBUG: true,
+        },
+      }),
+
       // Start the development server
       new ServerDevPlugin(),
     ],
@@ -94,6 +109,21 @@ export default [
       ],
     },
     plugins: [
+      new webpack.DefinePlugin({
+        // We're not running on the server
+        SERVER: false,
+        'process.env': {
+          // Point the server host/port to the dev server
+          HOST: JSON.stringify(process.env.HOST || 'localhost'),
+          PORT: JSON.stringify(process.env.PORT || '8081'),
+          SSL_PORT: process.env.SSL_PORT ? JSON.stringify(process.env.SSL_PORT) : null,
+
+          // Debug development
+          NODE_ENV: JSON.stringify('development'),
+          DEBUG: true,
+        },
+      }),
+
       // Check for errors, and refuse to emit anything with issues
       new webpack.NoEmitOnErrorsPlugin(),
 
