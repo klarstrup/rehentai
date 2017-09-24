cf1fca2def9d8ac57d79fea646b2f48fdf9a6760
klarstrup <jonas@klarstrup.dk>
2017-09-24 18:54:04 +0200

    check in pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    // First application
+    {
+      name: 'Re-Hentai Sever',
+      script: 'server.js',
+      env: {
+        HOST: 'h.klarstrup.dk',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      key: 'C:/Users/Klarstrup/.ssh/github_rsa',
+      user: 'root',
+      host: '46.101.225.235',
+      ref: 'origin/master',
+      repo: 'git@github.com:klarstrup/rehentai.git',
+      path: '/var/www/h.klarstrup.dk',
+      ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
+      'post-deploy': 'yarn install --ignore-engines && yarn build && /root/.nvm/versions/node/v8.5.0/bin/pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
